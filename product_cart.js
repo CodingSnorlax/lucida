c520a1d0ace588ace337a587b9d978ad8e129cb0
Karen Huang <thinknfix2021@gmail.com>
2022-02-17 16:48:03 +0800

    環境問題

---
+++ product_cart.js
@@ -0,0 +1,19 @@
+const app = {
+
+    data(){
+        return{
+            test: '測試'
+        }
+    },
+
+    methods: {
+
+    },
+
+    mounted(){
+        
+    }
+
+}
+
+Vue.createApp(app).mount('#app');
